d4b89df096b5b87f2fa9a5935da7ae14c5c318cb
arun-singh <arunbahra123@gmail.com>
2017-03-13 00:35:25 +0000

    Extend map viewer to locate link targets when clicked

--- src/GUI/JMapViewerExtension.java
+++ src/GUI/JMapViewerExtension.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import Graph.Grid;
+import Graph.Link;
 import Graph.Node;
 import javafx.scene.paint.*;
 import org.openstreetmap.gui.jmapviewer.*;
@@ -27,20 +28,43 @@ public class JMapViewerExtension extends JMapViewer{
             public void mouseClicked(MouseEvent e) {
                 Coordinate c = map.getPosition(e.getPoint());
                 MapMarkerDot source = nearest(c);
-                MapMarkerDot target = (MapMarkerDot) Map.getInstance().getMarkerPairs()
-                        .stream()
-                        .filter(m->m[0].equals(source))
-                        .collect(Collectors.toList())
-                        .get(0)[1];
+                Node nsource = new Node(source.getLat(), source.getLon());
+                System.out.println(source.getLat() + ", " + source.getLon());
 
-                if(source==null||target==null)
-                    System.out.println("null");
+                List<Link> targetList = Map.getInstance().getGrid()
+                        .getLinkMap()
+                        .entrySet()
+                        .stream()
+                        .filter(m->m.getValue().getSource().equals(nsource))
+                        .map(l->l.getValue())
+                        .collect(Collectors.toList());
+
+                if(targetList.size()==0)
+                    System.out.println("No Targets");
                 else{
-                    Color sourceColour = source.getBackColor().equals(Color.RED) ? Color.YELLOW : Color.RED;
-                    Color targetColour = source.getBackColor().equals(Color.RED) ? Color.YELLOW : Color.GREEN;
+                    System.out.println("Source has " + targetList.size() + " targets");
+                    Color sourceColour = source.getBackColor().equals(Color.GREEN) ? Color.YELLOW : Color.GREEN;
+                    System.out.println(sourceColour.toString());
                     source.setBackColor(sourceColour);
-                    target.setBackColor(targetColour);
                     repaint();
+
+                    for(int i = 0 ; i < targetList.size(); i++){
+                        double targetLat = targetList.get(i).getTarget().getLatitude();
+                        double targetLon = targetList.get(i).getTarget().getLongitude();
+                        for (int j = 0; j < getMapMarkerList().size(); j++) {
+                            if(getMapMarkerList().get(j).getLat() == targetLat && getMapMarkerList().get(j).getLon()==targetLon){
+                                System.out.print("Found");
+                                List<MapMarker> l = getMapMarkerList();
+                                MapMarkerDot ret= (MapMarkerDot) getMapMarkerList().get(j);
+                                Color targetColour = source.getBackColor().equals(Color.GREEN) ? Color.RED : Color.YELLOW;
+                                System.out.println(targetColour.toString());
+                                ret.setBackColor(targetColour);
+                                map.repaint();
+                                map.revalidate();
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         };
@@ -73,14 +97,6 @@ public class JMapViewerExtension extends JMapViewer{
                 .get();
         return mmd;
     }
-
-    public Node[] findTargetMarker(List<Node[]> pairs, Coordinate target){
-        List<Node[]> pair =  pairs.stream()
-                .filter(n->n[0].getLatitude()==target.getLat() && n[0].getLongitude()==target.getLon())
-                .collect(Collectors.toList());
-        return pair.size() != 0 ? pair.get(0) : null;
-    }
-
 
     public static double getNodesDistance(Coordinate startCoord, Coordinate endCoord) {
         double crowFliesLength;
