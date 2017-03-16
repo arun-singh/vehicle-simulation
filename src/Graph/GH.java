cf84f1e5d0f52e7527a61e4ac5b86d8d714147e1
arun-singh <arunbahra123@gmail.com>
2017-03-16 03:09:39 +0000

    Tried to snap Graphopper instructions to links - unccessfully

--- src/Graph/GH.java
+++ src/Graph/GH.java
@@ -1,21 +1,32 @@
 package Graph;
 
+import GUI.*;
+import Statistics.Statistics;
 import com.graphhopper.GHRequest;
 import com.graphhopper.GHResponse;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.PathWrapper;
 import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.util.Instruction;
 import com.graphhopper.util.InstructionList;
 import com.graphhopper.util.PointList;
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 
-import java.util.Locale;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Arun on 12/03/2017.
  */
 public class GH {
+    static Random ran = new Random();
+    static GraphHopper hopper = new GraphHopper();
     public static void request(){
-        GraphHopper hopper = new GraphHopper();
         hopper.setOSMFile("/Users/Arun/Documents/MOJO-Simulation/Resources/Birmingham.osm");
         hopper.setEncodingManager(new EncodingManager("car"));
         hopper.setGraphHopperLocation("../Resources/gh");
@@ -23,7 +34,7 @@ public class GH {
         hopper.importOrLoad();
 
 
-        GHRequest req = new GHRequest(52.4415506, -1.9325445, 52.4422945, -1.9280491).
+        GHRequest req = new GHRequest(52.4470355, -1.9352142, 52.4406603, -1.9253879).
                 setWeighting("fastest").
                 setVehicle("car").
                 setLocale(Locale.UK);
@@ -39,21 +50,147 @@ public class GH {
         PathWrapper path = rsp.getBest();
 
         PointList pointList = path.getPoints();
+        System.out.println("Point size: " + pointList.size());
         double distance = path.getDistance();
         long timeInMs = path.getTime();
 
         for(int i = 0; i <pointList.size(); i++){
             double lat = pointList.getLat(i);
             double lon = pointList.getLon(i);
-            System.out.println(lat + ", " + lon);
         }
 
-
         InstructionList il = path.getInstructions();
-//// iterate over every turn instruction
-//        for(Instruction instruction : il) {
-//            instruction.getDistance();
-//   ...
-//        }
+        int ilcount = 0;
+        for(Instruction instruction : il) {
+            instruction.getDistance();
+            System.out.println("---------------------");
+            System.out.println(instruction.getSign());
+            ilcount+=instruction.getPoints().size();
+            for (int i = 0; i < instruction.getPoints().size(); i++) {
+                System.out.println(instruction.getPoints().getLatitude(i) + ", " + instruction.getPoints().getLongitude(i));
+            }
+        }
+        System.out.println("Instruction point total: " + ilcount);
     }
+
+    public static List<List<Link>> generateRoutes(HashMap<Integer, Link> linkMap, int routeTotal){
+        hopper.setOSMFile("/Users/Arun/Documents/MOJO-Simulation/Resources/Birmingham.osm");
+        hopper.setEncodingManager(new EncodingManager("car"));
+        hopper.setGraphHopperLocation("../Resources/gh");
+        hopper.importOrLoad();
+
+        List<List<Link>> routes = new ArrayList<>();
+        List<Link> inputLinks = Statistics.getInputLinks(linkMap);
+
+        for(int i =0; i <routeTotal; i++){
+            int startingIndex = ran.nextInt((inputLinks.size()-1) + 1);
+            Link startingLink = inputLinks.get(startingIndex);
+            Node startingNode = startingLink.getTarget();
+            Node endingNode = pickEndLocation(inputLinks, startingLink);
+            InstructionList iList = getGHRoute(hopper, startingNode, endingNode);
+            drawInstructions(iList);
+
+            routes.add(ghToLink(startingLink, endingNode, iList));
+        }
+        return routes;
+    }
+
+    private static Node pickEndLocation(List<Link> inputLinks, Link starting){
+        inputLinks.remove(starting);
+         List<Link> minimumDistance = inputLinks.stream()
+                                                .filter(l-> MapUtil.getNodesDistance(new Coordinate(l.getSource().getLatitude(), l.getSource().getLongitude()),
+                                                        new Coordinate(starting.getSource().getLatitude(), starting.getSource().getLongitude())) > 500)
+                                                .collect(Collectors.toList());
+
+        int ranIndex = ran.nextInt((minimumDistance.size()-1) + 1);
+        return minimumDistance.get(ranIndex).getSource();
+    }
+
+    private static InstructionList getGHRoute(GraphHopper hopper, Node start, Node end){
+        GHRequest req = new GHRequest(start.getLatitude(), start.getLongitude(), end.getLatitude(), end.getLongitude()).
+                setWeighting("fastest").
+                setVehicle("car").
+                setLocale(Locale.UK);
+        GHResponse rsp = hopper.route(req);
+
+        if(rsp.hasErrors()) {
+            // handle them!
+            // rsp.getErrors()
+            System.out.println("errors");
+        }
+
+        PathWrapper path = rsp.getBest();
+        return path.getInstructions();
+    }
+
+
+    private static double distanceFromTarget(Instruction instruction, Link target){
+        double sum = 0;
+        List<Coordinate> coords = target.getPolyline().getCoordinates();
+        PointList points = instruction.getPoints();
+        for (int i = 0; i < points.size(); i++) {
+            for(Coordinate c : coords){
+                sum += MapUtil.getNodesDistance(new Coordinate(points.getLat(i), points.getLon(i)), new Coordinate(c.getLat(), c.getLon()));
+            }
+        }
+        return sum;
+    }
+
+    private static List<Link> ghToLink(Link start, Node end, InstructionList iList){
+        List<Link> route = new ArrayList<>();
+        List<Coordinate> coords = new ArrayList<>();
+
+
+        HashMap<Integer, Link> linkMap = GUI.Map.getInstance().getGrid().getLinkMap();
+        Node chosen = start.getTarget();
+        for(int i = 0; i <iList.size(); i++){
+            Instruction firstInstr = iList.get(i);
+            List<Link> targets = MapUtil.getTargetLinks(linkMap, chosen, route);
+
+            //Optional<Link> newLink = targets.stream().reduce((t1,t2)->distanceFromTarget(firstInstr, t1) < distanceFromTarget(firstInstr, t2) ? t1 : t2);
+            double avgDis = 100000;
+            Link newLink = null;
+            for(Link t : targets){
+                double dis = distanceFromTarget(firstInstr, t);
+                if(dis<avgDis) {
+                    avgDis = dis;
+                    newLink = t;
+                }
+            }
+
+//            if(!newLink.isPresent()){
+//                continue;
+//            }
+            chosen = newLink.getTarget();
+
+            //draw new link
+            Coordinate c = new Coordinate(newLink.getSource().getLatitude(), newLink.getSource().getLongitude());
+            Coordinate c1 = new Coordinate(newLink.getTarget().getLatitude(), newLink.getTarget().getLongitude());
+            coords.add(c); coords.add(c1);
+            route.add(newLink);
+        }
+        MapPolyLine line = new MapPolyLine(coords);
+        line.setBackColor(Color.BLUE);
+        line.setColor(Color.BLUE);
+        GUI.Map.getInstance().getMap().addMapPolygon(line);
+        return route;
+    }
+
+    public synchronized static void drawInstructions(InstructionList iList){
+        List<Coordinate> coords = new ArrayList<>();
+        List<MapMarker> markers = new ArrayList<>();
+        for(Instruction instr : iList){
+            PointList pl = instr .getPoints();
+            for (int i = 0; i < pl.size(); i++) {
+                MapMarkerDot md = new MapMarkerDot(pl.getLat(i), pl.getLon(i));
+                markers.add(md);
+            }
+        }
+        GUI.Map.getInstance().getMap().setMapMarkerList(markers);
+    }
+
+    public static void main(String[] args){
+        GH.request();
+    }
+
 }
--- src/Graph/MapUtil.java
+++ src/Graph/MapUtil.java
@@ -1,8 +1,12 @@
 package Graph;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -24,6 +28,22 @@ public class MapUtil {
                 .collect(Collectors.toList());
     }
 
+    public static List<Link> getTargetLinks(HashMap<Integer, Link> map, Node source, List<Link> route){
+        List<Node> sources;
+        Predicate<Link> pred;
+        if(route.size()>1){
+            sources = route.stream().map(l->l.getSource()).collect(Collectors.toList());
+            pred = l -> l.getSource().equals(source) && !sources.contains(l.getTarget());
+        }else{
+            pred = l -> l.getSource().equals(source);
+        }
+
+        return map.entrySet().stream()
+                .filter(l->pred.test(l.getValue()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
     public static boolean isUnconnected(List<Node[]> pairs, Node[] pair){
         return getSourceLinks(pairs, pair[0]).size() == 0
                 && getTargetLinks(pairs, pair[1]).size() == 0;
@@ -35,15 +55,6 @@ public class MapUtil {
                 .collect(Collectors.toList());
     }
 
-    public static void filter(List<Node[]> _new, List<Node[]> _removed){
-       for(Node[] rem : _removed){
-           Node remSource = rem[0];
-           // Any target nodes remaining that that had this node as source?
-           List<Node[]> target = getTargetLinks(_new, remSource);
-           System.out.println(target.size());
-           Node targetRemoved = rem[0];
-       }
-    }
 
     public static List<Integer> cache = new ArrayList<>();
     public static int connections(Link source){
@@ -60,6 +71,47 @@ public class MapUtil {
         if(filtered.size()==0)
             return 0;
         return servers.size() + filtered.stream().mapToInt(s->connections(s.getOutgoing())).sum();
+    }
+
+
+    public static boolean isLinkOneWay(Link link, HashMap<Integer, Link> linkMap){
+        Node source = link.getSource();
+        Node target = link .getTarget();
+        return linkMap.entrySet()
+                .stream()
+                .filter(l->l.getValue().getSource().equals(target) && l.getValue().getTarget().equals(source))
+                .map(l->l.getValue())
+                .collect(Collectors.toList())
+                .size() == 0;
+    }
+
+    public static List<Link> getInputLinks(HashMap<Integer, Link> linkMap, int averageConnectivity){
+        return linkMap.entrySet().stream()
+                .filter(l->l.getValue().getServers().size()>1 && l.getValue().getConnectivity()>averageConnectivity)
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public static double getNodesDistance(Coordinate startCoord, Coordinate endCoord) {
+        double crowFliesLength;
+        double R = 6371000; // Earths radius
+        // get lat and longs
+        double latI = startCoord.getLat();
+        double latI2 = endCoord.getLat();
+        double lngI = startCoord.getLon();
+        double lngI2 = endCoord.getLon();
+        // calculate change in lat and longs in rads
+        double dLat = Math.toRadians(latI2 - latI);
+        double dLng = Math.toRadians(lngI2 - lngI);
+        // Use 'Haversine' method to convert lat/long into distance is
+        // metres
+        double a = (Math.sin(dLat / 2) * Math.sin(dLat / 2))
+                + (Math.cos(Math.toRadians(latI)) * Math.cos(Math
+                .toRadians(latI2)))
+                * (Math.sin(dLng / 2) * Math.sin(dLng / 2));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        crowFliesLength = R * c;
+        return crowFliesLength;
     }
 
 
