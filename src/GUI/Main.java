2aa93e38d3f482b136ed5cbd9e9d2ce72977c61b
arun-singh <arunbahra123@gmail.com>
2017-03-18 16:45:13 +0000

    Option for stats mode

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -3,7 +3,9 @@ package GUI;
 import Graph.Grid;
 import Graph.Node;
 import Simulation.Simulate;
+import Statistics.Statistics;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
@@ -20,6 +22,7 @@ import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created by Arun on 17/01/2017.
@@ -31,6 +34,7 @@ public class Main extends Application{
     static Stage primaryStage;
 
     double maxLat, minLat, maxLon, minLon;
+    boolean statsMode = false;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -41,10 +45,10 @@ public class Main extends Application{
         Map map = Map.getInstance();
         //TODO: Get box coords
         java.util.List<double[]> latlon = new ArrayList<>();
-//        latlon.add(new double[]{52.4432354, -1.9366254});
-//        latlon.add(new double[]{52.437009, -1.9293171});
-//        latlon.add(new double[]{52.4469844, -1.9277054});
-//        latlon.add(new double[]{52.4407134, -1.9255364});
+        latlon.add(new double[]{52.4432354, -1.9366254});
+        latlon.add(new double[]{52.437009, -1.9293171});
+        latlon.add(new double[]{52.4469844, -1.9277054});
+        latlon.add(new double[]{52.4407134, -1.9255364});
 
         //52.422039, -1.813235
         //52.421541, -1.776757
@@ -60,16 +64,16 @@ public class Main extends Application{
         gp.add(m, 0, 0);
         gp.add(control, 0, 1);
 
-        latlon.add(new double[]{52.422039, -1.813235});
-        latlon.add(new double[]{52.421541, -1.776757});
-        latlon.add(new double[]{52.411569, -1.774139});
-        latlon.add(new double[]{52.404448, -1.813321});
+//        latlon.add(new double[]{52.422039, -1.813235});
+//        latlon.add(new double[]{52.421541, -1.776757});
+//        latlon.add(new double[]{52.411569, -1.774139});
+//        latlon.add(new double[]{52.404448, -1.813321});
         maxLat = Grid.maxLat(latlon);
         minLat = Grid.minLat(latlon);
         minLon = Grid.minLon(latlon);
         maxLon = Grid.maxLon(latlon);
 
-        map.getMap().setDisplayPosition(new Coordinate(52.422039, -1.813235), 15);
+        map.getMap().setDisplayPosition(new Coordinate(52.4407134, -1.9255364), 15);
 
         Scene scene = new Scene(gp);
         Main.primaryStage.setScene(scene);
@@ -77,30 +81,61 @@ public class Main extends Application{
     }
 
     public void ex(javafx.event.ActionEvent event){
-        Grid grid = new Grid(maxLat, minLat, maxLon, minLon);
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 Map.getInstance().update();
-                System.out.println("Hello !!!");
             }
         };
 
         Timer timer = new Timer();
         long delay = 0;
-        long intevalPeriod = 500;
+        long intevalPeriod = 100;
 
         timer.scheduleAtFixedRate(task, delay,
                 intevalPeriod);
 
         Thread thread = new Thread(){
             public void run(){
-                Simulate simulate = new Simulate(grid);
-                simulate.run();
+                if(!statsMode) {
+                    Grid grid = new Grid(maxLat, minLat, maxLon, minLon);
+                    Simulate simulate = new Simulate(grid);
+                    simulate.run();
+                }else {
+                    List<List<Statistics>> stats = Statistics.increaseCars(100, 1500, 100, 10,
+                            new double[]{maxLat, minLat, maxLon, minLon});
+
+                    Platform.runLater(() -> {
+                        drawGraphs(stats);
+                    });
+                }
             }
         };
         thread.start();
     }
+
+    public void drawGraphs(List<List<Statistics>> stats){
+        Stage graphs = new Stage();
+        graphs.setWidth(WIDTH);
+        graphs.setHeight(HEIGHT);
+
+        VBox time = Statistics.drawCarStats(stats, "Total number of cars", "Simulation time");
+        VBox shockwaves  = Statistics.drawShockwaves(stats, "Total number of cars", "Shockwaves");
+        VBox vehiclesRemaining = Statistics.vehiclesRemaining(stats, "Time", "Vehicles remaining");
+        VBox shockwaveSingle = Statistics.drawShockwaveSingleJourney(stats, "Time", "Shockwaves generated");
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(time, 0, 0);
+        gridPane.add(shockwaves, 0, 1);
+        gridPane.add(vehiclesRemaining, 1, 0);
+        gridPane.add(shockwaveSingle, 1, 1);
+
+
+        Scene scene = new Scene(gridPane);
+        graphs.setScene(scene);
+        graphs.show();
+    }
+
     public static void main(String[] args){
         launch(args);
     }
